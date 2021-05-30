9d0d9ef07e10cebef71cc87dad078d013977dd07
eunjae0337 <85046527+eunjae0337@users.noreply.github.com>
2021-05-30 12:46:24 +0900

    version 0.0

---
+++ p_psy_pro_4-1.py
@@ -0,0 +1,15 @@
+mass_1 = form[mass_1].value
+mass_2 = form[mass_2].value
+velocity_1 = form[velocity_1].value
+velocity_2 = form[velocity_2].value
+
+F_1 = 1/2 * (mass_1)**2 * (velocity_1)**2
+F_2 = 1/2 * (mass_2)**2 * (velocity_2)**2
+
+floor(F_1)
+floor(F_2)
+
+ceil(F_1)
+ceil(F_2)
+
+
